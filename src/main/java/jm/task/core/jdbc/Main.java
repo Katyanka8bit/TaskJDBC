44237158039e955462500a974121f023157c0e22
Екатерина Соседова <katrinasosedova2013@yandex.ru>
2021-06-20 19:09:58 +0300

    Test1

--- src/main/java/jm/task/core/jdbc/Main.java
+++ src/main/java/jm/task/core/jdbc/Main.java
@@ -1,7 +1,30 @@
 package jm.task.core.jdbc;
 
+import jm.task.core.jdbc.model.User;
+import jm.task.core.jdbc.service.UserService;
+import jm.task.core.jdbc.service.UserServiceImpl;
+
+import java.util.List;
+
 public class Main {
     public static void main(String[] args) {
-        // реализуйте алгоритм здесь
+        UserService u1 = new UserServiceImpl();
+
+        u1.createUsersTable();
+
+        u1.saveUser("Ivan", "Petrov", (byte) 25);
+        u1.saveUser("Ilya", "Petrov", (byte) 56);
+        u1.saveUser("Maria", "Petrova", (byte) 99);
+        u1.saveUser("Katya", "Sosedova", (byte) 21);
+
+        List<User> list = u1.getAllUsers();
+        for (User u : list) {
+            System.out.println(u.toString());
+        }
+
+        u1.cleanUsersTable();
+
+        u1.dropUsersTable();
     }
 }
+
--- src/main/java/jm/task/core/jdbc/util/Util.java
+++ src/main/java/jm/task/core/jdbc/util/Util.java
@@ -1,5 +1,28 @@
 package jm.task.core.jdbc.util;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.logging.Logger;
+
 public class Util {
-    // реализуйте настройку соеденения с БД
+    private static final Logger log = Logger.getLogger(Util.class.getName());
+    private static final String DRIVER = "com.mysql.cj.jdbc.Driver";
+    private static final String URL = "jdbc:mysql://localhost:3306/my_db?serverTimezone=UTC";
+    private static final String USERNAME = "root";
+    private static final String PASSWORD = "root";
+
+    public static Connection getMySQLConnection() {
+
+        Connection connection = null;
+
+        try {
+            Class.forName(DRIVER);
+            connection = DriverManager.getConnection(URL, USERNAME, PASSWORD);
+
+        } catch (SQLException | ClassNotFoundException e) {
+            e.printStackTrace();
+        }
+        return connection;
+    }
 }
